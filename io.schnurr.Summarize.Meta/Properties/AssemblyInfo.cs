e776b4da671911cb56008d0fa2f44be8e8460177
Samuel Schnurr <samuelschnurr@live.de>
2022-01-31 11:55:46 +0100

    Setup assembly info and project settings

--- io.schnurr.Summarize.Meta/Properties/AssemblyInfo.cs
+++ io.schnurr.Summarize.Meta/Properties/AssemblyInfo.cs
@@ -1,33 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("io.schnurr.Summarize.Meta.Properties")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Outlook Mail Summarize Meta")]
+[assembly: AssemblyDescription("Outlook addin to summarize mails with artificial intelligence Meta Project")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("io.schnurr.Summarize.Meta.Properties")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCompany("io.schnurr")]
+[assembly: AssemblyProduct("Summarize Meta")]
+[assembly: AssemblyCopyright("Samuel Schnurr © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("333bdce3-b899-44ed-a8ce-b22b66521027")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- io.schnurr.Summarize.Web/Properties/AssemblyInfo.cs
+++ io.schnurr.Summarize.Web/Properties/AssemblyInfo.cs
@@ -1,35 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Outlook Mail Summarize")]
 [assembly: AssemblyDescription("Outlook addin to summarize mails with artificial intelligence")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("io.schnurr")]
 [assembly: AssemblyProduct("Summarize")]
-[assembly: AssemblyCopyright("SamuelSchnurr ©  2022")]
+[assembly: AssemblyCopyright("Samuel Schnurr © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("865113b2-8554-4dca-b038-21fb2f5a53e7")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
