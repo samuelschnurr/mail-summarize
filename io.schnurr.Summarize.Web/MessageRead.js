aab849dd9d2a3817b9fa46531421b579a36c96bd
Samuel Schnurr <samuelschnurr@live.de>
2022-02-03 14:13:42 +0100

    Added first wip working summarize implementation

--- io.schnurr.Summarize.Web/MessageRead.js
+++ io.schnurr.Summarize.Web/MessageRead.js
@@ -1,90 +1,38 @@
-﻿(function () {
-  "use strict";
+﻿'use strict';
 
-  var messageBanner;
+(function () {
+    Office.initialize = function () {
+        $(document).ready(function () {
+            $("#doWork").click(function () {
+                test();
+            });
+        });
+    };
 
-  // The Office initialize function must be run each time a new page is loaded.
-  Office.initialize = function (reason) {
-    $(document).ready(function () {
-      var element = document.querySelector('.MessageBanner');
-      messageBanner = new components.MessageBanner(element);
-      messageBanner.hideBanner();
-      loadProps();
-    });
-  };
-
-  // Take an array of AttachmentDetails objects and build a list of attachment names, separated by a line-break.
-  function buildAttachmentsString(attachments) {
-    if (attachments && attachments.length > 0) {
-      var returnString = "";
-      
-      for (var i = 0; i < attachments.length; i++) {
-        if (i > 0) {
-          returnString = returnString + "<br/>";
-        }
-        returnString = returnString + attachments[i].name;
-      }
-
-      return returnString;
+    function test() {
+        Office.context.mailbox.item.body.getAsync(
+            Office.CoercionType.Text,
+            { asyncContext: "This is passed to the callback" },
+            function callback(result) {
+                summarizeTest(result)
+            });
     }
 
-    return "None";
-  }
+    function summarizeTest(text) {
+        var requestUrl = 'http://localhost:7071/api/SummarizeFunction';
 
-  // Format an EmailAddressDetails object as
-  // GivenName Surname <emailaddress>
-  function buildEmailAddressString(address) {
-    return address.displayName + " &lt;" + address.emailAddress + "&gt;";
-  }
-
-  // Take an array of EmailAddressDetails objects and
-  // build a list of formatted strings, separated by a line-break
-  function buildEmailAddressesString(addresses) {
-    if (addresses && addresses.length > 0) {
-      var returnString = "";
-
-      for (var i = 0; i < addresses.length; i++) {
-        if (i > 0) {
-          returnString = returnString + "<br/>";
-        }
-        returnString = returnString + buildEmailAddressString(addresses[i]);
-      }
-
-      return returnString;
+        $.ajax({
+            url: requestUrl,
+            dataType: 'text',
+            data: "{ 'text': '"+text.value +"'}",
+            contentType: 'application/json',
+            type: 'POST',
+            error: function (xhr, status, error) {
+                console.log("SummarizeTest", error);
+            }
+        }).done(function (res) {
+            console.log("SummarizeTest", res);
+            $("#selectedText").text(res);
+        });
     }
-
-    return "None";
-  }
-
-  // Load properties from the Item base object, then load the
-  // message-specific properties.
-  function loadProps() {
-    var item = Office.context.mailbox.item;
-
-    $('#dateTimeCreated').text(item.dateTimeCreated.toLocaleString());
-    $('#dateTimeModified').text(item.dateTimeModified.toLocaleString());
-    $('#itemClass').text(item.itemClass);
-    $('#itemId').text(item.itemId);
-    $('#itemType').text(item.itemType);
-
-    $('#message-props').show();
-
-    $('#attachments').html(buildAttachmentsString(item.attachments));
-    $('#cc').html(buildEmailAddressesString(item.cc));
-    $('#conversationId').text(item.conversationId);
-    $('#from').html(buildEmailAddressString(item.from));
-    $('#internetMessageId').text(item.internetMessageId);
-    $('#normalizedSubject').text(item.normalizedSubject);
-    $('#sender').html(buildEmailAddressString(item.sender));
-    $('#subject').text(item.subject);
-    $('#to').html(buildEmailAddressesString(item.to));
-  }
-
-  // Helper function for displaying notifications
-  function showNotification(header, content) {
-    $("#notificationHeader").text(header);
-    $("#notificationBody").text(content);
-    messageBanner.showBanner();
-    messageBanner.toggleExpansion();
-  }
 })();
