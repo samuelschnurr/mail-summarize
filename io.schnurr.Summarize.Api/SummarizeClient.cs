69c31434f359544d50f939064519de53fd9773de
Samuel Schnurr <samuelschnurr@live.de>
2022-02-08 10:18:14 +0100

    Added initial vscode azure function files

--- io.schnurr.Summarize.Api/SummarizeClient.cs
+++ io.schnurr.Summarize.Api/SummarizeClient.cs
@@ -7,58 +7,58 @@ using System.Threading.Tasks;
 
 namespace io.schnurr.Summarize.Api
 {
-    internal class SummarizeClient : TextAnalyticsClient
-    {
-        internal SummarizeClient(Uri endpoint, AzureKeyCredential credentials) : base(endpoint, credentials)
-        { }
+	internal class SummarizeClient : TextAnalyticsClient
+	{
+		internal SummarizeClient(Uri endpoint, AzureKeyCredential credentials) : base(endpoint, credentials)
+		{ }
 
-        internal async Task<string> SummarizeTextAsync(List<string> batchInput)
-        {
-            TextAnalyticsActions actions = new TextAnalyticsActions()
-            {
-                ExtractSummaryActions = new List<ExtractSummaryAction>() { new ExtractSummaryAction() }
-            };
+		internal async Task<string> SummarizeTextAsync(List<string> batchInput)
+		{
+			TextAnalyticsActions actions = new TextAnalyticsActions()
+			{
+				ExtractSummaryActions = new List<ExtractSummaryAction>() { new ExtractSummaryAction() }
+			};
 
-            AnalyzeActionsOperation operation = await StartAnalyzeActionsAsync(batchInput, actions);
+			AnalyzeActionsOperation operation = await StartAnalyzeActionsAsync(batchInput, actions);
 
-            await operation.WaitForCompletionAsync();
+			await operation.WaitForCompletionAsync();
 
-            StringBuilder sb = new StringBuilder();
+			StringBuilder sb = new StringBuilder();
 
-            await foreach (AnalyzeActionsResult documentsInPage in operation.Value)
-            {
-                IReadOnlyCollection<ExtractSummaryActionResult> summaryResults = documentsInPage.ExtractSummaryResults;
+			await foreach (AnalyzeActionsResult documentsInPage in operation.Value)
+			{
+				IReadOnlyCollection<ExtractSummaryActionResult> summaryResults = documentsInPage.ExtractSummaryResults;
 
-                foreach (ExtractSummaryActionResult summaryActionResults in summaryResults)
-                {
-                    if (summaryActionResults.HasError)
-                    {
-                        sb.AppendLine("Error!");
-                        sb.AppendLine($"Action error code: {summaryActionResults.Error.ErrorCode}.");
-                        sb.AppendLine($"Message: {summaryActionResults.Error.Message}");
-                        continue;
-                    }
+				foreach (ExtractSummaryActionResult summaryActionResults in summaryResults)
+				{
+					if (summaryActionResults.HasError)
+					{
+						sb.AppendLine("Error!");
+						sb.AppendLine($"Action error code: {summaryActionResults.Error.ErrorCode}.");
+						sb.AppendLine($"Message: {summaryActionResults.Error.Message}");
+						continue;
+					}
 
-                    foreach (ExtractSummaryResult documentResults in summaryActionResults.DocumentsResults)
-                    {
-                        if (documentResults.HasError)
-                        {
-                            sb.AppendLine($"Error!");
-                            sb.AppendLine($"Document error code: {documentResults.Error.ErrorCode}.");
-                            sb.AppendLine($"Message: {documentResults.Error.Message}");
-                            continue;
-                        }
+					foreach (ExtractSummaryResult documentResults in summaryActionResults.DocumentsResults)
+					{
+						if (documentResults.HasError)
+						{
+							sb.AppendLine($"Error!");
+							sb.AppendLine($"Document error code: {documentResults.Error.ErrorCode}.");
+							sb.AppendLine($"Message: {documentResults.Error.Message}");
+							continue;
+						}
 
-                        foreach (SummarySentence sentence in documentResults.Sentences)
-                        {
-                            sb.AppendLine($"{sentence.Text}");
-                            sb.AppendLine();
-                        }
-                    }
-                }
-            }
+						foreach (SummarySentence sentence in documentResults.Sentences)
+						{
+							sb.AppendLine($"{sentence.Text}");
+							sb.AppendLine();
+						}
+					}
+				}
+			}
 
-            return sb.ToString();
-        }
-    }
+			return sb.ToString();
+		}
+	}
 }
