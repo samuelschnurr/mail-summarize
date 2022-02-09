f2df755742a6dcd81a0c51bdf46ba804199f9c6e
Samuel Schnurr <samuelschnurr@live.de>
2022-02-09 20:34:11 +0100

    Did npm lint fix

--- io.schnurr.Summarize.Web/src/commands/commands.ts
+++ io.schnurr.Summarize.Web/src/commands/commands.ts
@@ -1,46 +1,37 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-/* global global, Office, self, window */
-
 Office.onReady(() => {
-  // If needed, Office.js is ready to be called
-});
+    // If needed, Office.js is ready to be called
+})
 
 /**
  * Shows a notification when the add-in command is executed.
  * @param event
  */
 function action(event: Office.AddinCommands.Event) {
-  const message: Office.NotificationMessageDetails = {
-    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-    message: "Performed action.",
-    icon: "Icon.80x80",
-    persistent: true,
-  };
+    const message: Office.NotificationMessageDetails = {
+        type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+        message: "Performed action.",
+        icon: "Icon.80x80",
+        persistent: true,
+    }
 
-  // Show a notification message
-  Office.context.mailbox.item?.notificationMessages.replaceAsync("action", message);
+    // Show a notification message
+    Office.context.mailbox.item?.notificationMessages.replaceAsync("action", message)
 
-  const x = "";
-
-  // Be sure to indicate when the add-in command function is complete
-  event.completed();
+    // Be sure to indicate when the add-in command function is complete
+    event.completed()
 }
 
 function getGlobal() {
-  return typeof self !== "undefined"
-    ? self
-    : typeof window !== "undefined"
-    ? window
-    : typeof global !== "undefined"
-    ? global
-    : undefined;
+    return typeof self !== "undefined"
+        ? self
+        : typeof window !== "undefined"
+        ? window
+        : typeof global !== "undefined"
+        ? global
+        : undefined
 }
 
-const g = getGlobal() as any;
+const g = getGlobal() as any
 
 // The add-in command functions need to be available in global scope
-g.action = action;
+g.action = action
--- io.schnurr.Summarize.Web/src/taskpane/index.tsx
+++ io.schnurr.Summarize.Web/src/taskpane/index.tsx
@@ -1,41 +1,39 @@
-import App from "./components/App";
-import { AppContainer } from "react-hot-loader";
-import { initializeIcons } from "@fluentui/font-icons-mdl2";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { ThemeProvider } from "@fluentui/react";
+import { initializeIcons } from "@fluentui/font-icons-mdl2"
+import { ThemeProvider } from "@fluentui/react"
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import { AppContainer } from "react-hot-loader"
+
+import App from "./components/App"
 
 /* global document, Office, module, require */
 
-initializeIcons();
+initializeIcons()
 
-let isOfficeInitialized = false;
+let isOfficeInitialized = false
 
-const title = "Contoso Task Pane Add-in";
-const x = "HI";
-
-const a = "";
+const title = "Hello"
 
 const render = (Component: any) => {
-  ReactDOM.render(
-    <AppContainer>
-      <ThemeProvider>
-        <Component title={title} isOfficeInitialized={isOfficeInitialized} />
-      </ThemeProvider>
-    </AppContainer>,
-    document.getElementById("container")
-  );
-};
+    ReactDOM.render(
+        <AppContainer>
+            <ThemeProvider>
+                <Component title={title} isOfficeInitialized={isOfficeInitialized} />
+            </ThemeProvider>
+        </AppContainer>,
+        document.getElementById("container")
+    )
+}
 
 /* Render application after Office initializes */
 Office.onReady(() => {
-  isOfficeInitialized = true;
-  render(App);
-});
+    isOfficeInitialized = true
+    render(App)
+})
 
 if ((module as any).hot) {
-  (module as any).hot.accept("./components/App", () => {
-    const NextApp = require("./components/App").default;
-    render(NextApp);
-  });
+    ;(module as any).hot.accept("./components/App", () => {
+        const NextApp = require("./components/App").default
+        render(NextApp)
+    })
 }
