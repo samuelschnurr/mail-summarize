3b4a8de3f655394b97be601caca229217dcb3549
Samuel Schnurr <samuelschnurr@live.de>
2022-02-14 16:37:31 +0100

    Added PlainAnalyzeActionsResult

---
+++ io.schnurr.Summarize.Api/PlainAnalyzeActionsResult.cs
@@ -0,0 +1,8 @@
+namespace io.schnurr.Summarize.Api
+{
+    internal class PlainAnalyzeActionsResult
+    {
+        internal string Summary { get; set; }
+        internal string Sentiment { get; set; }
+    }
+}
