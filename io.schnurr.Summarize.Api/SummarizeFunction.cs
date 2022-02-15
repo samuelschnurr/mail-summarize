a55c76b53f40fc79eadf4dcab508491032b3f06a
Samuel Schnurr <samuelschnurr@live.de>
2022-02-15 08:47:47 +0100

    Cleaned up SummarizeFunction

--- io.schnurr.Summarize.Api/SummarizeFunction.cs
+++ io.schnurr.Summarize.Api/SummarizeFunction.cs
@@ -4,45 +4,53 @@ using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using Azure;
+using Azure.AI.TextAnalytics;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 
 namespace io.schnurr.Summarize.Api
 {
-	public static class SummarizeFunction
-	{
-		private static readonly string AzureEndpoint = Environment.GetEnvironmentVariable(nameof(AzureEndpoint), EnvironmentVariableTarget.Process);
-		private static readonly string AzureKeyCredential = Environment.GetEnvironmentVariable(nameof(AzureKeyCredential), EnvironmentVariableTarget.Process);
+    public static class SummarizeFunction
+    {
+        private static readonly string AzureEndpoint = Environment.GetEnvironmentVariable(nameof(AzureEndpoint), EnvironmentVariableTarget.Process);
+        private static readonly string AzureKeyCredential = Environment.GetEnvironmentVariable(nameof(AzureKeyCredential), EnvironmentVariableTarget.Process);
 
-		[Function("SummarizeFunction")]
-		public async static Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req,
-			FunctionContext executionContext)
-		{
-			string requestBody = string.Empty;
+        [Function("SummarizeFunction")]
+        public async static Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
+        {
+            string requestBody = string.Empty;
 
-			using (var streamReader = new StreamReader(req.Body))
-			{
-				requestBody = await streamReader.ReadToEndAsync();
-			}
+            using (var streamReader = new StreamReader(req.Body))
+            {
+                requestBody = await streamReader.ReadToEndAsync();
+            }
 
-			if (string.IsNullOrWhiteSpace(requestBody))
-			{
-				var errorResponse = req.CreateResponse(HttpStatusCode.BadRequest);
-				return errorResponse;
-			}
+            if (string.IsNullOrWhiteSpace(requestBody))
+            {
+                var errorResponse = req.CreateResponse(HttpStatusCode.BadRequest);
+                return errorResponse;
+            }
 
-			var batchInput = new List<string>() { requestBody };
+            var credentials = new AzureKeyCredential(AzureKeyCredential);
+            var endpoint = new Uri(AzureEndpoint);
+            var client = new SummarizeClient(endpoint, credentials);
 
-			var credentials = new AzureKeyCredential(AzureKeyCredential);
-			var endpoint = new Uri(AzureEndpoint);
-			var client = new SummarizeClient(endpoint, credentials);
+            var actions = GetTextAnalyticsActions(true);
+            var batchInput = new List<string>() { requestBody };
+            var plainResults = await client.GetPlainAnalyzeActionsResultsAsync(actions, batchInput);
 
-			string summarizedText = await client.SummarizeTextAsync(batchInput);
+            var response = req.CreateResponse();
+            await response.WriteAsJsonAsync(plainResults);
+            return response;
+        }
 
-			var response = req.CreateResponse(HttpStatusCode.OK);
-			response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-			await response.WriteStringAsync(summarizedText);
-			return response;
-		}
-	}
+        private static TextAnalyticsActions GetTextAnalyticsActions(bool disableServiceLogs)
+        {
+            return new TextAnalyticsActions()
+            {
+                AnalyzeSentimentActions = new List<AnalyzeSentimentAction>() { new AnalyzeSentimentAction() { IncludeOpinionMining = true, DisableServiceLogs = disableServiceLogs } },
+                ExtractSummaryActions = new List<ExtractSummaryAction>() { new ExtractSummaryAction() { DisableServiceLogs = disableServiceLogs } }
+            };
+        }
+    }
 }
