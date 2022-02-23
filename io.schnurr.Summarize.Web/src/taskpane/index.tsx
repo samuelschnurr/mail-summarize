96b1bf467b707ad557d4006233e37a5904b8306e
Samuel Schnurr <samuelschnurr@live.de>
2022-02-23 10:48:58 +0100

    Cleaned up index.tsx

--- io.schnurr.Summarize.Web/src/taskpane/index.tsx
+++ io.schnurr.Summarize.Web/src/taskpane/index.tsx
@@ -5,14 +5,11 @@ import * as ReactDOM from "react-dom"
 import { AppContainer } from "react-hot-loader"
 
 import App from "./components/App"
-
-/* global document, Office, module, require */
-
 initializeIcons()
 
 let isOfficeInitialized = false
 
-const title = "Hello"
+const title = "Mail Summarize"
 
 const render = (Component: any) => {
     ReactDOM.render(
@@ -25,7 +22,8 @@ const render = (Component: any) => {
     )
 }
 
-/* Render application after Office initializes */
+// Imported javascript api via cdn in taskpane.html
+// eslint-disable-next-line no-undef
 Office.onReady(() => {
     isOfficeInitialized = true
     render(App)
@@ -33,7 +31,6 @@ Office.onReady(() => {
 
 if ((module as any).hot) {
     ;(module as any).hot.accept("./components/App", () => {
-        const NextApp = require("./components/App").default
-        render(NextApp)
+        render(App)
     })
 }
