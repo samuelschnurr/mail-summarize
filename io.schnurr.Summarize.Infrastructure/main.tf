6b856aa061f9628c448d24040449aa2fba043192
Samuel Schnurr <samuelschnurr@live.de>
2022-03-01 15:33:29 +0100

    Added provider configuration with variables

--- io.schnurr.Summarize.Infrastructure/main.tf
+++ io.schnurr.Summarize.Infrastructure/main.tf
@@ -10,6 +10,10 @@ terraform {
 }
 
 provider "azurerm" {
+  subscription_id = var.SUBSCRIPTION_ID
+  client_id       = var.SP_CLIENT_ID
+  client_secret   = var.SP_CLIENT_SECRET
+  tenant_id       = var.SP_TENANT_ID
   features {}
 }
 
--- io.schnurr.Summarize.Infrastructure/variables.tf
+++ io.schnurr.Summarize.Infrastructure/variables.tf
@@ -9,3 +9,25 @@ variable "APPLICATION_NAME" {
   type        = string
   default     = "mailsummarize"
 }
+
+# These variables need to be set to instantiate the azure ressources
+# Enter default values for local development but dont commit them to git
+variable "SUBSCRIPTION_ID" {
+  description = "Subscription ID of the Azure subscription"
+  type        = string
+}
+
+variable "SP_CLIENT_ID" {
+  description = "Service Principal Client ID"
+  type        = string
+}
+
+variable "SP_CLIENT_SECRET" {
+  description = "Service Principal Client Secret"
+  type        = string
+}
+
+variable "SP_TENANT_ID" {
+  description = "Service Principal Tenant ID"
+  type        = string
+}
