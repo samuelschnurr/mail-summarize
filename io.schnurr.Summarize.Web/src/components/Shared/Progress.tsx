f2e0f55488bf9f540bf27d0dba371c125af6a499
Samuel Schnurr <samuelschnurr@live.de>
2022-03-06 16:39:39 +0100

    Made props on progress component optional

--- io.schnurr.Summarize.Web/src/components/Shared/Progress.tsx
+++ io.schnurr.Summarize.Web/src/components/Shared/Progress.tsx
@@ -2,21 +2,39 @@ import { Spinner, SpinnerSize } from "@fluentui/react"
 import * as React from "react"
 
 export interface ProgressProps {
-    logo: string
+    logo?: string
+    title?: string
     message: string
-    title: string
 }
 
 const Progress = (props: ProgressProps) => {
     return (
         <section className="ms-welcome__progress ms-u-fadeIn500">
-            <img width="90" height="90" src={props.logo} alt={props.title} title={props.title} />
-            <h1 className="ms-fontSize-su ms-fontWeight-light ms-fontColor-neutralPrimary">
-                {props.title}
-            </h1>
+            {renderLogo(props)}
+            {renderTitle(props)}
             <Spinner size={SpinnerSize.large} label={props.message} />
         </section>
     )
+}
+
+const renderLogo = (props: ProgressProps) => {
+    if (props.logo && props.title) {
+        return <img width="90" height="90" src={props.logo} alt={props.title} title={props.title} />
+    }
+
+    return <></>
+}
+
+const renderTitle = (props: ProgressProps) => {
+    if (props.message) {
+        return (
+            <h1 className="ms-fontSize-su ms-fontWeight-light ms-fontColor-neutralPrimary">
+                {props.title}
+            </h1>
+        )
+    }
+
+    return <></>
 }
 
 export default Progress
