9b90d59197b2bf25059e978ff5b9c5a9bb57daf3
Samuel Schnurr <samuelschnurr@live.de>
2022-03-06 16:42:48 +0100

    Cleaned up Summary component

--- io.schnurr.Summarize.Web/src/components/Summary/Summary.tsx
+++ io.schnurr.Summarize.Web/src/components/Summary/Summary.tsx
@@ -1,4 +1,5 @@
 import { DefaultButton } from "@fluentui/react"
+import Progress from "components/Shared/Progress"
 import * as React from "react"
 import { useEffect, useState } from "react"
 import { getMailInput } from "services/officeService"
@@ -20,17 +21,22 @@ const Summary = () => {
         )
     }, [])
 
+    if(!mailInput.body && !mailInput.error){
+        return <Progress message="Loading" />
+    }
+
     return (
         <main className="ms-welcome__main">
             <h2 className="ms-font-xl ms-fontWeight-semilight ms-fontColor-neutralPrimary ms-u-slideUpIn20">
-                {mailInput.body + "--" + mailInput.error}
+                {mailInput.error}
             </h2>
+            {!mailInput.error && (
             <DefaultButton
                 className="ms-welcome__action"
                 iconProps={{ iconName: "ChevronRight" }}
                 onClick={() => alert("Clicked!")}>
-                Show more
-            </DefaultButton>
+                Extend summary
+            </DefaultButton>)}
         </main>
     )
 }
