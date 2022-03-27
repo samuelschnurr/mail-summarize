2f6ed18b4a808adcfb8d94947645d20cf65988c8
Samuel Schnurr <samuelschnurr@live.de>
2022-03-27 13:49:55 +0200

    Implemented mailsummary in addin

--- io.schnurr.Summarize.Web/src/components/Summary/index.tsx
+++ io.schnurr.Summarize.Web/src/components/Summary/index.tsx
@@ -2,12 +2,18 @@ import { DefaultButton } from "@fluentui/react"
 import Progress from "components/Shared/Progress"
 import * as React from "react"
 import { useEffect, useState } from "react"
+import { analyzeMail } from "services/cognitiveService"
 import { getMailInput } from "services/officeService"
 
 const Summary = () => {
     const [mailInput, setMailInput] = useState({
         body: "",
         error: "",
+    })
+
+    const [mailSummary, setMailSummary] = useState({
+        summary: "",
+        sentiment: "",
     })
 
     useEffect(() => {
@@ -21,23 +27,40 @@ const Summary = () => {
         )
     }, [])
 
+    useEffect(() => {
+        if (mailInput.body) {
+            analyzeMail(mailInput.body).then(result => {
+                setMailSummary({
+                    summary: result.data[0].Summary,
+                    sentiment: result.data[0].Sentiment,
+                })
+            })
+        }
+    }, [mailInput.body])
+
+    // Loading input
     if (!mailInput.body && !mailInput.error) {
+        return <Progress message="Loading" />
+    }
+
+    // Loading summary
+    if (mailInput.body && !mailSummary.summary && !mailSummary.sentiment) {
         return <Progress message="Loading" />
     }
 
     return (
         <main className="ms-welcome__main">
             <h2 className="ms-font-xl ms-fontWeight-semilight ms-fontColor-neutralPrimary ms-u-slideUpIn20">
-                {mailInput.error}
+                {mailInput.error || mailSummary.summary}
             </h2>
-            {!mailInput.error && (
-                <DefaultButton
-                    className="ms-welcome__action"
-                    iconProps={{ iconName: "ChevronRight" }}
-                    onClick={() => alert("Clicked!")}>
-                    Extend summary
-                </DefaultButton>
-            )}
+            <DefaultButton
+                className="ms-welcome__action"
+                iconProps={{ iconName: "ChevronRight" }}
+                onClick={() => {
+                    console.log("Extend")
+                }}>
+                Extend summary
+            </DefaultButton>
         </main>
     )
 }
