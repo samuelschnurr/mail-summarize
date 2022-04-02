b59daeec46ac7eac71381f6bc163fcb28ea79ab1
Samuel Schnurr <samuelschnurr@live.de>
2022-04-02 16:09:48 +0200

    Added header padding class

--- io.schnurr.Summarize.Web/src/components/Base/Header.tsx
+++ io.schnurr.Summarize.Web/src/components/Base/Header.tsx
@@ -8,9 +8,9 @@ export interface HeaderProps {
 
 const Header = (props: HeaderProps) => {
     return (
-        <section className="ms-welcome__header ms-bgColor-neutralLighter ms-u-fadeIn500">
-            <img width="90" height="90" src={props.logo} alt={props.title} title={props.title} />
-            <h1 className="ms-fontSize-su ms-fontWeight-light ms-fontColor-neutralPrimary">
+        <section className="ms-welcome__header ms-bgColor-neutralLighter ms-u-fadeIn500 header-padding">
+            <img width="30" height="30" src={props.logo} alt={props.title} title={props.title} />
+            <h1 className="ms-fontSize-xl ms-fontWeight-light ms-fontColor-neutralPrimary">
                 {props.message}
             </h1>
         </section>
