b594423f3cd8dacbe9fdc414ee440e4ef019bed6
Samuel Schnurr <samuelschnurr@live.de>
2022-04-23 20:14:15 +0200

    Added functionkey to axios header

--- io.schnurr.Summarize.Web/src/appsetup/axios.ts
+++ io.schnurr.Summarize.Web/src/appsetup/axios.ts
@@ -3,6 +3,11 @@ import axios from "axios"
 const initializeAxios = () => {
     axios.defaults.baseURL = process.env.API_URL
     axios.defaults.headers.post["Content-Type"] = "application/json"
+
+    if (process.env.NODE_ENV === "production") {
+        // Currently only set inline - extract to separate key vault
+        axios.defaults.headers.common["code"] = "<function_key>"
+    }
 }
 
 export default initializeAxios
