ead639b9fa9200b27d465878fecdf8ec94c51f6f
Samuel Schnurr <samuelschnurr@live.de>
2022-04-23 18:34:30 +0200

    Changed blob url prod

--- io.schnurr.Summarize.Web/webpack.config.js
+++ io.schnurr.Summarize.Web/webpack.config.js
@@ -5,7 +5,7 @@ import webpack from "webpack"
 import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin"
 
 const appUrlDev = "https://localhost:3000/"
-const appUrlProd = "https://app-mailsummarize.azurewebsites.net/"
+const appUrlProd = "https://stamailsummarizeapp.z6.web.core.windows.net/"
 const apiUrlDev = "http://localhost:7071/api"
 const apiUrlProd = "https://fa-mailsummarize.azurewebsites.net/api"
 
