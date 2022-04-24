9fbbbc54b34a8dd34b6fc50451ed0edb335fbdc7
Samuel Schnurr <samuelschnurr@live.de>
2022-04-24 11:37:19 +0200

    Removed serviceprincipal variables

--- io.schnurr.summarize.infrastructure/main.tf
+++ io.schnurr.summarize.infrastructure/main.tf
@@ -16,10 +16,6 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = var.SUBSCRIPTION_ID
-  client_id       = var.SP_CLIENT_ID
-  client_secret   = var.SP_CLIENT_SECRET
-  tenant_id       = var.SP_TENANT_ID
   features {}
 }
 
--- io.schnurr.summarize.infrastructure/variables.tf
+++ io.schnurr.summarize.infrastructure/variables.tf
@@ -9,25 +9,3 @@ variable "APPLICATION_NAME" {
   type        = string
   default     = "mailsummarize"
 }
-
-# These variables need to be set to instantiate the azure ressources
-# Set here as default but dont commit - or set as environment variables
-variable "SUBSCRIPTION_ID" {
-  description = "Subscription ID of the Azure subscription"
-  type        = string
-}
-
-variable "SP_CLIENT_ID" {
-  description = "Service Principal Client ID"
-  type        = string
-}
-
-variable "SP_CLIENT_SECRET" {
-  description = "Service Principal Client Secret"
-  type        = string
-}
-
-variable "SP_TENANT_ID" {
-  description = "Service Principal Tenant ID"
-  type        = string
-}
