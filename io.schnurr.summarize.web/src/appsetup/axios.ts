20de89bbcf00cdb0ace54d450ae25f2d1b96446c
Samuel Schnurr <samuelschnurr@live.de>
2022-04-24 12:25:01 +0200

    Extended keyvault auth problem comment

--- io.schnurr.summarize.web/src/appsetup/axios.ts
+++ io.schnurr.summarize.web/src/appsetup/axios.ts
@@ -5,7 +5,9 @@ const initializeAxios = () => {
     axios.defaults.headers.post["Content-Type"] = "application/json"
 
     if (process.env.NODE_ENV === "production") {
-        // Currently only set inline - extract to separate key vault
+        // Accessing key vault is not available from clientside in the browser.
+        // Also CORS is not available in key vault from client side.
+        // So the function key is set here directly.
         axios.defaults.headers.common["code"] = "<function_key>"
     }
 }
